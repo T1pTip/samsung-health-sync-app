8f7620e76d4636b6e1e4fe22f17c5ff4dbee4ab9
T1pTip <T1pTip@users.noreply.github.com>
2026-05-22 21:19:51 +0300

    Bump versionCode to 2 so APK installs as update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.moran.healthsync"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
 
     buildTypes {
